8d36a8a1f0debb349ae602c2a3fa5c20e7faf53f
luabagg <luanbaggio0@gmail.com>
2024-09-08 11:54:58 -0300

    fix: add footer to layout.

--- app/components/layout/Layout.tsx
+++ app/components/layout/Layout.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import Navbar from './Navbar';
 import RouteChangeAnnouncement from './RouteChangeAnnouncement';
+import Footer from './Footer';
 
 export function Layout({ children }: { children: React.ReactNode }) {
     return (
         <React.Fragment>
             <Navbar />
             {children}
-            {/* <Footer /> */}
+            <Footer />
             <RouteChangeAnnouncement />
         </React.Fragment>
     );
--- app/components/layout/RouteChangeAnnouncement.tsx
+++ app/components/layout/RouteChangeAnnouncement.tsx
@@ -1,7 +1,7 @@
+import * as React from "react";
 import {
   useLocation,
 } from "@remix-run/react";
-import * as React from "react";
 
 /**
  * Provides an alert for screen reader users when the route changes.
