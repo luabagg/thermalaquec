65c761fdbb6ab7c3ae7866c607f20eee0c71b1d4
luabagg <luanbaggio0@gmail.com>
2024-09-09 12:19:12 -0300

    fix: add missing file types.

---
+++ app/components/layout/types.tsx
@@ -0,0 +1,4 @@
+import type { SvgIconComponent } from "@mui/icons-material";
+
+export type NavigationMap = Array<{ name: string, href: string }>;
+export type SocialMap = Array<{ icon: SvgIconComponent, href: string }>;
